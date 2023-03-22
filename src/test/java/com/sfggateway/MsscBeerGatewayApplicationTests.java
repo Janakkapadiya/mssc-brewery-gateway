f4b4b7ef1275a69adfb3c1adab72266f3d503232
Janak <jaykapadiya8899@gmail.com>
2023-03-22 12:25:38 +0530

    initial commit

---
+++ src/test/java/com/sfggateway/MsscBeerGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfggateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
